c267f87698ed5c194db4def1a707d2b7c47e791b
joaovpt <joaotassi18@gmail.com>
2025-07-09 15:05:10 -0300

    feat: add Email consumer and RabbitMQ configuration

---
+++ email/src/main/java/com/estudos/email/configuration/RabbitMq.java
@@ -0,0 +1,17 @@
+package com.estudos.email.configuration;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMq {
+
+    private final String queueName = "email-queue";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName, true);
+    }
+
+}
